30b54258272bcd610f60f5519e27655b72293769
Juan Vasquez <juanvcas96@gmail.com>
2022-11-27 10:56:46 -0500

    [ADD] Lazy loading code and the register log. [ADD] Some styles to images background.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Import function to add to the intersection observer event.
 import { registerImage } from './lazy';
 
 // Elements
@@ -8,23 +9,26 @@ const cleanButton = document.querySelector('#clean-images');
 // Load an image
 const loadImage = async () => {
    const image = document.createElement('img');
-   image.classList = 'w-full';
    // image.src = await (
    //    await (await fetch('https://randomfox.ca/floof/')).json()
    // ).image;
-   image.src = `https://randomfox.ca/images/${Math.floor(
-      Math.random() * 123
-   )}.jpg`;
+   // image.src = `https://randomfox.ca/images/${Math.floor(
+   //    Math.random() * (123 - 1)
+   // ) + 1}.jpg`;
+
+   image.dataset.src = `https://randomfox.ca/images/${
+      Math.floor(Math.random() * (123 - 1)) + 1
+   }.jpg`;
    image.alt = 'randomfox';
 
    const figure = document.createElement('figure');
-   figure.classList = 'w-96 mx-auto mt-8';
+   figure.classList = 'w-96 h-96 mx-auto mt-8 bg-gray-300 rounded-md';
 
    figure.append(image);
    imagesCont.append(figure);
 
-   // Lazy
-   registerImage(image);
+   // Lazy loading
+   registerImage(figure);
 };
 
 //Clean images
--- src/lazy.js
+++ src/lazy.js
@@ -1,19 +1,49 @@
-const action = (entry) => {
-   const node = entry.target;
+let totalImages = 0;
+let loadedImages = 0;
 
-   console.log('hey!');
-   observer.unobserve(node);
+const action = (entry) => {
+   // entry.target.firstChild.src = `https://randomfox.ca/images/${
+   //    Math.floor(Math.random() * (123 - 1)) + 1
+   // }.jpg`;
+
+   const image = entry.target.firstChild;
+
+   image.src = image.dataset.src;
+   image.alt = 'randomfox';
+   image.classList = 'w-full rounded-md';
+
+   image.addEventListener('load', () => {
+      entry.target.classList = 'w-96 mx-auto mt-8';
+   });
+
+   loadedImages++;
+   console.log(`
+   ----------------------------
+   🔵 Total images: ${totalImages}
+   🟢 Loaded images: ${loadedImages}
+   ----------------------------
+   `);
+
+   observer.unobserve(entry.target);
 };
 
 const observer = new IntersectionObserver((entry) => {
    entry.filter((info) => info.isIntersecting).forEach(action);
 
-   /* Tambien puede ser */
+   /* Also it can be */
    // if (entry[0].isIntersecting) {
    //    action(entry[0]);
    // }
 });
 
-export const registerImage = (image) => {
-   observer.observe(image);
+export const registerImage = (figure) => {
+   observer.observe(figure);
+
+   totalImages++;
+   console.log(`
+   ----------------------------
+   🔵 Total images: ${totalImages}
+   🟢 Loaded images: ${loadedImages}
+   ----------------------------
+   `);
 };
