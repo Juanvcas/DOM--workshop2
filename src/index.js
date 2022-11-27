498907b7095d001d9e546a9c1d94dd2ae585f42c
Juan Vasquez <juanvcas96@gmail.com>
2022-11-26 22:08:18 -0500

    [ADD] The image generator code. [ADD] The Intersection observer code, but still not lazy loading.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from './lazy';
 
-console.log('Happy hacking :)')
+// Elements
+const imagesCont = document.querySelector('#images');
+const loadButton = document.querySelector('#load-image');
+const cleanButton = document.querySelector('#clean-images');
+
+// Load an image
+const loadImage = async () => {
+   const image = document.createElement('img');
+   image.classList = 'w-full';
+   // image.src = await (
+   //    await (await fetch('https://randomfox.ca/floof/')).json()
+   // ).image;
+   image.src = `https://randomfox.ca/images/${Math.floor(
+      Math.random() * 123
+   )}.jpg`;
+   image.alt = 'randomfox';
+
+   const figure = document.createElement('figure');
+   figure.classList = 'w-96 mx-auto mt-8';
+
+   figure.append(image);
+   imagesCont.append(figure);
+
+   // Lazy
+   registerImage(image);
+};
+
+//Clean images
+const cleanImages = () => {
+   imagesCont.innerHTML = '';
+};
+
+// Events
+loadButton.addEventListener('click', loadImage);
+cleanButton.addEventListener('click', cleanImages);
